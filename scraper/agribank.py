650958ed08b39e28bc7e7b0af22a04dff08588c8
Minh Do <131603843+minhdnn@users.noreply.github.com>
2025-06-30 21:08:20 +0700

    Update agribank.py

--- scraper/agribank.py
+++ scraper/agribank.py
@@ -6,9 +6,8 @@ def get_agribank_rates():
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
 
-    table = soup.find("table", class_="table")  # class name may vary
-    rows = table.find_all("tr")[1:]  # Skip header
-
+    table = soup.find("table", class_="table")
+    rows = table.find_all("tr")[1:]
     target_currencies = ['USD', 'EUR', 'JPY', 'CNY']
     rates = []
 
@@ -24,14 +23,13 @@ def get_agribank_rates():
         try:
             buy = float(cols[1].text.replace(',', '').strip())
             sell = float(cols[3].text.replace(',', '').strip())
+            rates.append({
+                'bank': 'Agribank',
+                'currency': currency,
+                'buy': buy,
+                'sell': sell
+            })
         except:
             continue
-
-        rates.append({
-            'bank': 'Agribank',
-            'currency': currency,
-            'buy': buy,
-            'sell': sell
-        })
 
     return rates
